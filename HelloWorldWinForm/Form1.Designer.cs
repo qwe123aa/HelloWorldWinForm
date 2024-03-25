5d0b19d82b36d3f56d8150d08efb8df9474dc9ad
thdud__ <sylim1114y@gmail.com>
2024-03-25 14:27:50 +0900

    form 제목 변경

--- HelloWorldWinForm/Form1.Designer.cs
+++ HelloWorldWinForm/Form1.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(705, 450);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Form111111";
             this.ResumeLayout(false);
 
         }
